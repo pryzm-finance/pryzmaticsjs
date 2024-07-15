cda7fb42e8143ffd097d72ff453a84aaa10ca691
Amin Abbasi <amin@refractedlabs.com>
2024-07-15 17:30:52 +0330

    Upgrade pryzmaticsjs for examples [skip ci]

--- example/src/pool.ts
+++ example/src/pool.ts
@@ -5,10 +5,11 @@ import { PRYZMATICS_ENDPOINT } from "./constants";
 async function main() {
     const pryzmaticsClient = await createPryzmaticsClient({ restEndpoint: PRYZMATICS_ENDPOINT })
 
-    const pool = (await pryzmaticsClient.pryzmatics.pool({
+    const pool = await pryzmaticsClient.pryzmatics.pool({
         poolId: 2n
-    })).pool
-    console.log(pool)
+    })
+    console.log(pool.pool)
+    console.log(pool.zero_impact_join_capabilities)
 }
 
 main().catch(console.error)
