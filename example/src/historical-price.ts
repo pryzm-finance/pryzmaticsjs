df285e270debf27b294a5046392dbb1db095ed43
Amin Abbasi <amin@refractedlabs.com>
2025-06-03 08:42:49 +0000

    chore: adapt example

--- example/src/historical-price.ts
+++ example/src/historical-price.ts
@@ -12,7 +12,8 @@ async function main() {
         from: `${Date.parse('2023-07-27 11:59:33.619 GMT')}`,
         to: `${Date.parse('2023-07-27 12:10:04.045 GMT')}`,
         timeResolutionType: TimeResolutionType.TIME_RESOLUTION_TYPE_MINUTE,
-        timeResolutionValue: 5
+        timeResolutionValue: 5,
+        useExternalPriceSource: false
     })).historical_prices
     console.log(prices)
 
@@ -22,7 +23,20 @@ async function main() {
         from: `${Date.parse('2023-07-27 11:59:33.619 GMT')}`,
         to: `${Date.parse('2023-07-27 12:10:04.045 GMT')}`,
         timeResolutionType: TimeResolutionType.TIME_RESOLUTION_TYPE_MINUTE,
-        timeResolutionValue: 5
+        timeResolutionValue: 5,
+        useExternalPriceSource: false
+    })).historical_prices
+    console.log(prices)
+
+    const supportedDenoms = (await pryzmaticsClient.pryzmatics.config()).config.supported_denoms_for_external_price
+    prices = (await pryzmaticsClient.pryzmatics.historicalPrice({
+        denom: supportedDenoms[0],
+        quote: '',
+        from: `${Date.parse('2023-07-27 11:59:33.619 GMT')}`,
+        to: `${Date.parse('2023-07-27 12:10:04.045 GMT')}`,
+        timeResolutionType: TimeResolutionType.TIME_RESOLUTION_TYPE_MINUTE,
+        timeResolutionValue: 5,
+        useExternalPriceSource: true
     })).historical_prices
     console.log(prices)
 }
